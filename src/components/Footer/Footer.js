f70e5a3c3113338dc45f0a666af95b5339b712e7
IT21361036 <it21361036@my.sliit.lk>
2024-10-04 18:17:16 +0530

    updated the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,22 +7,7 @@ function Footer() {
   return (
     <footer className="footer">
       <Container fluid>
-        {/* <Nav>
-          <NavItem>
-            <NavLink href="">About Us</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="">Blog</NavLink>
-          </NavItem>
-        </Nav> */}
-        <div className="copyright">
-          © {new Date().getFullYear()} made with{" "}
-          <i className="tim-icons icon-heart-2" /> by{" "}
-          <a href="" target="_blank">
-            Vendora
-          </a>{" "}
-          for a better web.
-        </div>
+   
       </Container>
     </footer>
   );
